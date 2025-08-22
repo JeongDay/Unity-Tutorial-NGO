519639a93d33b7f1b765313a50007aff3b2b76c0
JeongDay <cidd1332@gmail.com>
2025-08-22 16:16:22 +0900

    Netcode for GameObject Init

---
+++ Assets/_Scripts/ScoreManager.cs
@@ -0,0 +1,36 @@
+﻿using TMPro;
+using Unity.Netcode;
+using UnityEngine;
+
+public class ScoreManager : NetworkBehaviour
+{
+    public static ScoreManager Instance;
+    
+    [SerializeField] private TextMeshProUGUI scoreTextUI;
+    private NetworkVariable<int> globalScore = new NetworkVariable<int>(0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
+    
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+        
+        globalScore.OnValueChanged += OnScoreChanged;
+    }
+    
+    private void OnScoreChanged(int prevValue, int newValue)
+    {
+        scoreTextUI.text = newValue.ToString();
+    }
+    
+    public void AddScore()
+    {
+        if (!IsServer)
+            return;
+        
+        globalScore.Value++;
+    }
+}
